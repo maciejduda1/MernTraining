e43422c1d2f62a640ca613b8434dfab59a4dadff
Maciej Duda <maciej.duda1@gazeta.pl>
2018-02-26 11:36:32 +0100

    localStorage

--- client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
+++ client/modules/Post/components/PostCreateWidget/PostCreateWidget.js
@@ -4,7 +4,52 @@ import { injectIntl, intlShape, FormattedMessage } from 'react-intl';
 // Import Style
 import styles from './PostCreateWidget.css';
 
+
+
 export class PostCreateWidget extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputTitle: '',
+      inputAuthor: '',
+      inputContent: '',
+    };
+  }
+
+  componentDidMount = () => {
+    this.setState({ inputAuthor: localStorage.getItem('Author') || 'Author' });
+    this.setState({ inputTitle: localStorage.getItem('Title') || 'Title' });
+    this.setState({ inputContent: localStorage.getItem('Content') || 'Content' });
+  }
+  componentDidUpdate = () => {
+  /*  console.log('inputTitle ', this.state.inputTitle);
+    console.log('inputName ', this.state.inputAuthor);
+    console.log('inputContent ', this.state.inputContent);
+    console.log('local', localStorage.getItem("Author"));*/
+    localStorage.setItem('Author', this.state.inputAuthor);
+    localStorage.setItem('Title', this.state.inputTitle);
+    localStorage.setItem('Content', this.state.inputContent);
+  }
+  handleChange = (event) => {
+    if (event.target.name === 'Title') {
+      this.setState({ inputTitle: event.target.value });
+    }
+    if (event.target.name === 'Author') {
+      this.setState({ inputAuthor: event.target.value });
+    }
+    if (event.target.name === 'Content') {
+      this.setState({ inputContent: event.target.value });
+    }
+  }
+  handleSubmit = () => {
+    localStorage.removeItem('Author');
+    localStorage.removeItem('Title');
+    localStorage.removeItem('Content');
+    console.log('consolouje usuniety tag', localStorage.getItem('Author'));
+    this.setState({ inputAuthor: 'Author' });
+    this.setState({ inputTitle: 'Title' });
+    this.setState({ inputContent: 'Content' });
+  }
   addPost = () => {
     const nameRef = this.refs.name;
     const titleRef = this.refs.title;
@@ -12,18 +57,19 @@ export class PostCreateWidget extends Component {
     if (nameRef.value && titleRef.value && contentRef.value) {
       this.props.addPost(nameRef.value, titleRef.value, contentRef.value);
       nameRef.value = titleRef.value = contentRef.value = '';
+      this.handleSubmit();
     }
   };
-
+  // this.props.intl.messages.authorName  defaultValue={localStorage.getItem("Author")}
   render() {
     const cls = `${styles.form} ${(this.props.showAddPost ? styles.appear : '')}`;
     return (
       <div className={cls}>
-        <div className={styles['form-content']}>
+        <div className={styles['form-content']} >
           <h2 className={styles['form-title']}><FormattedMessage id="createNewPost" /></h2>
-          <input placeholder={this.props.intl.messages.authorName} className={styles['form-field']} ref="name" />
-          <input placeholder={this.props.intl.messages.postTitle} className={styles['form-field']} ref="title" />
-          <textarea placeholder={this.props.intl.messages.postContent} className={styles['form-field']} ref="content" />
+          <input placeholder={this.state.inputAuthor} value={this.state.inputAuthor} className={styles['form-field']} ref="name" name="Author" onChange={this.handleChange} />
+          <input placeholder={this.props.intl.messages.postTitle} value={this.state.inputTitle} className={styles['form-field']} ref="title" name="Title" onChange={this.handleChange} />
+          <textarea placeholder={this.props.intl.messages.postContent} value={this.state.inputContent} className={styles['form-field']} ref="content" name="Content" onChange={this.handleChange} />
           <a className={styles['post-submit-button']} href="#" onClick={this.addPost}><FormattedMessage id="submit" /></a>
         </div>
       </div>
